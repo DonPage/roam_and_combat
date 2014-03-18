21dceadad52582203963553290dd04ab5200c906
DonPage <don.page.work@gmail.com>
2014-03-17 21:23:20 -0400

    done with map and player locations

--- files/map.js
+++ files/map.js
@@ -82,5 +82,154 @@ console.log("map loading...");
             ["g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g"]
         ];
 
+    var dirt = new tile("http://hailclinic.com/wp-content/uploads/2013/05/Dirt-00-seamless-300x300.jpg", 0, 0, tileW, tileH);
+    var grass = new tile("http://img.photobucket.com/albums/v207/worg121/Maps/grass_tile2_600x600.png", 0, 0, tileW, tileH);
+    var cobble = new tile("http://i.imgur.com/qRaMK.png", 0, 0, tileW, tileH);
+    var marble = new tile("http://t3.gstatic.com/images?q=tbn:ANd9GcQxsaJ2M2uI_dBA0vd1IHmT_uFO4Js7iIWqI_Fv3QplUeLUqhK5pA", 0, 0, tileW, tileH);
+    var water = new tile("http://opengameart.org/sites/default/files/brushwalker437.png", 0, 0, tileW, tileH);
+
+    var player = new rect(0,0, tileW, tileH);
+
+    (function animationLoop(){
+        requestAnimationFrame(animationLoop);
+
+        render();
+        update();
+    })();
+
+    function render(){
+        ctx.clearRect(0, 0, map.width, map.height);
+        drawMap();
+        drawRect(player);
+    }
+
+    function drawMap(){
+        for (var i=0;i<mapArray.length;i++){ //loop inside of loop to get to the 2nd dimtion of the map.
+            for (var j=0;j<mapArray[i].length;j++){
+                dirt.x = j * tileW;
+                dirt.y = i * tileH;
+                grass.x = j * tileW;
+                grass.y = i * tileH;
+                cobble.x = j * tileW;
+                cobble.y = i * tileH;
+                marble.x = j * tileW;
+                marble.y = i * tileH;
+                water.x = j * tileW;
+                water.y = i * tileH;
+
+                if (mapArray[i][j] === "d")
+                {
+                    drawTile(dirt);
+                }
+
+                if (mapArray[i][j] === "g")
+                {
+                    drawTile(grass);
+                }
+
+                if (mapArray[i][j] === "c")
+                {
+                    drawTile(cobble);
+                }
+
+                if (mapArray[i][j] === "m")
+                {
+                    drawTile(marble);
+                }
+
+                if (mapArray[i][j] === "w")
+                {
+                    drawTile(water);
+                }
+            }
+        }
+    }
+
+    var clock = new THREE.Clock();
+    var time = clock.getElapsedTime();
+    var delta = clock.getDelta() * 1000;
+
+    function update(){
+        delta = clock.getDelta() *1000;
+        document.querySelector('#fps').innerHTML = "FPS: " + 1000 / delta;
+
+        var column = (map.width - player.x) /tileW;
+        var row = (map.height - player.y) /tileH;
+        document.querySelector('#loc').innerHTML = "x: "+ column +" y: "+ row +"";
+
+        wallIsct(player);
+    }
+
+    document.onkeydown = function(evt){
+        evt = evt || window.event;
+        var charCode = evt.keyCode || evt.which;
+        var charStr = String.fromCharCode(charCode); //returns keydown as a string.
+
+        key = charStr; //charStr is now = key
+
+        if (key === "A")
+        {
+            player.x -= tileW;
+        }
+
+        if (key === "D")
+        {
+            player.x += tileW;
+        }
+
+        if (key === "W")
+        {
+            player.y -= tileH;
+        }
+
+        if (key === "S")
+        {
+            player.y += tileH;
+        }
+    };
+
+    document.onkeyup = function(){ //everytime a key is released it resets the variable 'key'.
+        key = "";
+    };
+
+    function tile(src, x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.image = new Image();
+        this.image.src = src;
+    }
+
+    function drawTile(object){
+        ctx.drawImage(object.image, object.x, object.y, object.width, object.height);
+    }
+
+    function rect(x, y, width, height, color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    function drawRect(object){
+        ctx.beginPath();
+        ctx.rect(object.x, object.y ,object.width, object.height);
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+    }
+
+    function wallIsct(object)
+    {
+        object.x = clamp(object.x, 0, map.width - object.width);
+        object.y = clamp(object.y, 0, map.height - object.height);
+    }
+
+    function clamp(i, min, max){
+        return Math.max(Math.min(i, max),min);
+    }
 
 })();
